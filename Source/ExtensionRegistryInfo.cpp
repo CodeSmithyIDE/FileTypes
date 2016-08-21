e8aa4a5979c5c4feade2d982da2873a845d07efd
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2016-08-21 17:05:31 +0100

    Fixed indentation

--- Source/ExtensionRegistryInfo.cpp
+++ Source/ExtensionRegistryInfo.cpp
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2015 Xavier Leclercq
+    Copyright (c) 2015-2016 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OROTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OROTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "ExtensionRegistryInfo.h"
@@ -33,33 +33,33 @@ ExtensionRegistryInfo::~ExtensionRegistryInfo()
 
 std::string ExtensionRegistryInfo::progID() const
 {
-	return m_key.getValueAsString("");
+    return m_key.getValueAsString("");
 }
 
 void ExtensionRegistryInfo::getOpenWithProgids(std::vector<std::string>& progIDs)
 {
-	try
-	{
-		WindowsRegistry::Key key = WindowsRegistry::Registry::openSubKey(m_key, "OpenWithProgids");
-		key.enumValues(progIDs);
-	}
-	catch (const WindowsRegistry::KeyOperationException& e)
-	{
-		if (e.windowsErrorCode() != ERROR_FILE_NOT_FOUND)
-		{
-			throw;
-		}
-	}
+    try
+    {
+        WindowsRegistry::Key key = WindowsRegistry::Registry::openSubKey(m_key, "OpenWithProgids");
+        key.enumValues(progIDs);
+    }
+    catch (const WindowsRegistry::KeyOperationException& e)
+    {
+        if (e.windowsErrorCode() != ERROR_FILE_NOT_FOUND)
+        {
+            throw;
+        }
+    }
 }
 
 void ExtensionRegistryInfo::addOpenWithProgids(const std::string& progID)
 {
-	WindowsRegistry::Key key = WindowsRegistry::Registry::createSubKey(m_key, "OpenWithProgids");
-	key.setValue(progID, "");
+    WindowsRegistry::Key key = WindowsRegistry::Registry::createSubKey(m_key, "OpenWithProgids");
+    key.setValue(progID, "");
 }
 
 ExtensionRegistryInfo::ExtensionRegistryInfo(WindowsRegistry::Key& key)
-	: m_key(key)
+    : m_key(key)
 {
 }
 
--- Source/FileTypeAssociations.cpp
+++ Source/FileTypeAssociations.cpp
@@ -1,23 +1,23 @@
 /*
-	Copyright (c) 2015 Xavier Leclercq
+    Copyright (c) 2015-2016 Xavier Leclercq
 
-	Permission is hereby granted, free of charge, to any person obtaining a
-	copy of this software and associated documentation files (the "Software"),
-	to deal in the Software without restriction, including without limitation
-	the rights to use, copy, modify, merge, publish, distribute, sublicense,
-	and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OROTHER DEALINGS
-	IN THE SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OROTHER DEALINGS
+    IN THE SOFTWARE.
 */
 
 #include "FileTypeAssociations.h"
@@ -29,49 +29,49 @@ namespace FileTypes
 {
 
 ExtensionRegistryInfo FileTypeAssociations::createExtensionRegistryInfo(const std::string& extension,
-																		const std::string& progID)
+                                                                        const std::string& progID)
 {
-	std::string subKeyPath = "Software\\Classes\\" + extension;
-	WindowsRegistry::Key extKey = WindowsRegistry::Registry::createSubKey(HKEY_CURRENT_USER, subKeyPath);
-	extKey.setValue("", progID);
-	return ExtensionRegistryInfo(extKey);
+    std::string subKeyPath = "Software\\Classes\\" + extension;
+    WindowsRegistry::Key extKey = WindowsRegistry::Registry::createSubKey(HKEY_CURRENT_USER, subKeyPath);
+    extKey.setValue("", progID);
+    return ExtensionRegistryInfo(extKey);
 }
 
 void FileTypeAssociations::removeExtensionRegistryInfo(const std::string& extension)
 {
-	std::string subKeyPath = "Software\\Classes\\" + extension;
-	WindowsRegistry::Registry::deleteSubKey(HKEY_CURRENT_USER, subKeyPath);
+    std::string subKeyPath = "Software\\Classes\\" + extension;
+    WindowsRegistry::Registry::deleteSubKey(HKEY_CURRENT_USER, subKeyPath);
 }
 
 ExtensionRegistryInfo FileTypeAssociations::openExtensionRegistryInfo(const std::string& extension)
 {
-	std::string subKeyPath = "Software\\Classes\\" + extension;
-	WindowsRegistry::Key extKey = WindowsRegistry::Registry::openSubKey(HKEY_CURRENT_USER, subKeyPath);
-	return ExtensionRegistryInfo(extKey);
+    std::string subKeyPath = "Software\\Classes\\" + extension;
+    WindowsRegistry::Key extKey = WindowsRegistry::Registry::openSubKey(HKEY_CURRENT_USER, subKeyPath);
+    return ExtensionRegistryInfo(extKey);
 }
 
 ProgIDRegistryInfo FileTypeAssociations::createProgIDRegistryInfo(const std::string& progID,
-																  const std::string& applicationName)
+                                                                  const std::string& applicationName)
 {
-	std::string subKeyPath = "Software\\Classes\\" + progID;
-	WindowsRegistry::Key progIDKey = WindowsRegistry::Registry::createSubKey(HKEY_CURRENT_USER, subKeyPath);
+    std::string subKeyPath = "Software\\Classes\\" + progID;
+    WindowsRegistry::Key progIDKey = WindowsRegistry::Registry::createSubKey(HKEY_CURRENT_USER, subKeyPath);
 
-	progIDKey.setValue("", applicationName);
+    progIDKey.setValue("", applicationName);
 
-	return ProgIDRegistryInfo(progIDKey);
+    return ProgIDRegistryInfo(progIDKey);
 }
 
 void FileTypeAssociations::removeProgIDRegistryInfo(const std::string& progID)
 {
-	std::string subKeyPath = "Software\\Classes\\" + progID;
-	WindowsRegistry::Registry::deleteSubKey(HKEY_CURRENT_USER, subKeyPath);
+    std::string subKeyPath = "Software\\Classes\\" + progID;
+    WindowsRegistry::Registry::deleteSubKey(HKEY_CURRENT_USER, subKeyPath);
 }
 
 ProgIDRegistryInfo FileTypeAssociations::openProgIDRegistryInfo(const std::string& progID)
 {
-	std::string subKeyPath = "Software\\Classes\\" + progID;
-	WindowsRegistry::Key progIDKey = WindowsRegistry::Registry::openSubKey(HKEY_CURRENT_USER, subKeyPath);
-	return ProgIDRegistryInfo(progIDKey);
+    std::string subKeyPath = "Software\\Classes\\" + progID;
+    WindowsRegistry::Key progIDKey = WindowsRegistry::Registry::openSubKey(HKEY_CURRENT_USER, subKeyPath);
+    return ProgIDRegistryInfo(progIDKey);
 }
 
 }
